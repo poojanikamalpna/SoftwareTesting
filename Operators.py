8ddd67f1d359208e58ce8dc19e43b165887ed359
software <poojanikam378@gmail.com>
2023-02-13 09:25:12 +0530

    init

---
+++ Operators.py
@@ -0,0 +1,65 @@
+#Arthamatic operators
+#  (+,-,*,/)
+
+# print(10+10)
+# print(10-5)
+# print(10*5)
+# print(10/10)
+#  # Assingning a Variable
+
+# A= 10
+# B=30
+# print(A+B)
+
+# a=30
+# b=40
+# c=a+b
+# d=a-b
+# e=a*b
+# f=a/b
+# g=c/d
+# print(a,b,c,d,e,f,g)
+
+#=================================================================================================
+
+#  Logical Operators
+
+# a=True
+# b=True
+# print("a and b", a and b)
+# print("a or b",a or b)
+
+
+# a=True
+# b=False
+# print("a and b", a and b)
+# print("a or b",a or b)
+# a=False
+# b=False
+# print("a and b", a and b)
+# print("a or b",a or b)
+
+# a=False
+# b=True
+# print("a and b", a and b)
+# print("a or b",a or b)
+#=====================================================================================
+a=30
+b=40
+print("a==b",a==b)  # a=False  b=False   result= False
+print("a>=b",a>=b)  # a=False a=True    result=False
+print("a<=b",a<=b)  # a=True b=False   result=True
+print("a!=b",a!=b) #  a=True b=True    result=True
+
+#=====================================================================================
+
+a=30
+b=30
+print("a==b",a==b)
+print("a<=b",a<=b)
+print("a>=b",a>=b)
+print("a!=b",a!=b)
+
+
+
+
