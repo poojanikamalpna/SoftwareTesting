d9381682a9e9aa1fc39cc1d3f2187738341528ef
software <poojanikam378@gmail.com>
2023-02-13 09:27:21 +0530

    init

---
+++ Typecasting.py
@@ -0,0 +1,11 @@
+# a=10+10
+# print(a)
+
+# print(10 + "10")
+#TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+#print("10"+"10")
+
+avalue=10
+bvalue=20
+print(avalue + bvalue)
---
+++ variables.py
@@ -0,0 +1,28 @@
+# print("hello")
+# # lets begun with variables
+# a=10
+# print(a*10)
+# variables are just container to store value
+# a=100
+# print(type(a))
+
+# b=15.3
+# print(type(b))
+
+# c="Pooja"
+# print(type(c))
+
+# d=True
+# print(type(d))
+
+# e=bool(15)
+# print(e)
+
+# f='''hello Pooja
+#           you are going to select our campany
+#           as a executive officer
+#           glad to see you here'''
+# print(f)
+
+g=None
+print(g)
